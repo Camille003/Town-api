c4a4792b95a17ae33483c95f9d36f33cb6098365
camille <camilleseke03@gmail.com>
2019-07-24 15:10:39 +0100

    Made modification Package.json to run on dev mode

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 const express = require('express')
 
-const weatherRoutes = require("./routes/weather");
-const restaurantRoutes = require("./routes/restaurant");
+const weatherRoutes = require("./routers/weather");
+const restaurantRoutes = require("./routers/restaurant");
 
 const app = express();
 
 app.use(express.json());
 
-app.use(weatherRoutes);
-app.use(restaurantRoutes);
+// app.use(weatherRoutes);
+// app.use(restaurantRoutes);
 
 module.exports = app;
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
-app.listen(port ,(port)=>{
+app.listen(port ,()=>{
   console.log(`App is up and running on port ${port}`)
 })
